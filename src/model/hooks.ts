0abe69fc72dfa3e8fb061906a9971334d3f4b7c7
Konstantin Kovalev <constantiner@gmail.com>
2024-05-29 17:24:21 +0200

    feat: Create createAppAsyncThunkFactory to provide rejected value

--- src/model/hooks.ts
+++ src/model/hooks.ts
@@ -1,10 +1,13 @@
 // This file serves as a central hub for re-exporting pre-typed Redux hooks.
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, useStore } from "react-redux";
 import type { AppCreateAsyncThunkConfig, AppDispatch, AppStore, RootState } from "./store";
-import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
 export const createAppAsyncThunk = createAsyncThunk.withTypes<AppCreateAsyncThunkConfig>();
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const createAppAsyncThunkFactory = <ErrorType = unknown>() =>
+	createAsyncThunk.withTypes<AppCreateAsyncThunkConfig<ErrorType>>();
