f78f726af98b7d6b968d940ec5975899d86818bc
Konstantin Kovalev <constantiner@gmail.com>
2025-12-06 20:13:04 +0100

    fix: correct typo in header logo text

--- src/components/layout/header/headerLogo.tsx
+++ src/components/layout/header/headerLogo.tsx
@@ -9,7 +9,7 @@ export function HeaderLogo(): JSX.Element {
 		<Link href="/" className="mr-2 flex items-center">
 			<LogoSvg className="mr-1 size-8 md:size-10" />
 			<H1 family="mono" className="select-none" size="sm">
-				oktolityl
+				oktolity
 			</H1>
 		</Link>
 	);
